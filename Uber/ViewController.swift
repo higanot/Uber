42f01d21b2117c3f91db304559a22ef6fda1d04d
higanot <higanot555@gmail.com>
2018-09-04 20:15:57 +0900

    Initial Commit

---
+++ Uber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uber
+//
+//  Created by 日向野卓也 on 2018/09/04.
+//  Copyright © 2018年 takuya.higano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
